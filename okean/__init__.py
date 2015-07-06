4912008940be4523a3e2fa5a5a0a39eb87909fc1
Martinho MA <mma@staff-2158.wireless.ua.pt>
2015-07-06 12:27:26 +0100

    added roms glider ipython notebook

--- okean/__init__.py
+++ okean/__init__.py
@@ -9,4 +9,4 @@ contains general modeling tools. Specific tools are also included for ROMS.
 
 __authors__ = 'Martinho Marta-Almeida <m.martalmeida@gmail.com> \
 <Couto de Esteves, 3740-037, Portugal>',
-__version__='2015-07-06 11:08:43.572078'
+__version__='2015-07-06 12:26:31.119083'

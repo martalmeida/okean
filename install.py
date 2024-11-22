5c0ce7408bec5e9908e4aef43843690ad07b7b95
M Marta-Almeida <m.martalmeida@gmail.com>
2024-11-22 11:24:14 +0000

    fixed fortran line continuation in rtools

--- install.py
+++ install.py
@@ -9,7 +9,7 @@ import subprocess
 def go():
   here=os.path.dirname(__file__)
 
-  dest=os.path.join(site.getsitepackages()[0],'okean_TESTE')
+  dest=os.path.join(site.getsitepackages()[0],'okean')
   if os.path.isdir(dest):
     print('## folder exists: %s'%dest)
     shutil.rmtree(dest)
@@ -23,20 +23,22 @@ def go():
     print('  -- copying %-20s to %s'%(fsrc,dest))
     shutil.copy2(fsrc,dest)
 
-  # copy bin folder:
+  # copy contents of bin folder to bin:
   src=os.path.join(here,'okean','bin')
-  dest_=os.path.join(dest,'bin')
-  print('## copying %s to %s'%(src,dest))
-  shutil.copytree(src,dest_)
-  # replace 1st line
-  files=os.listdir(dest_)
+  dest_=os.path.dirname(sys.executable)
+  files=glob.glob(os.path.join(src,'*'))
   for f in files:
-    f=os.path.join(dest_,f)
-    lines=open(f).readlines()
+    print('## copying %s to %s'%(f,dest_))
+    shutil.copy2(f,dest_)
+
+    # replace 1st line
+    ff=os.path.join(dest_,os.path.basename(f))
+
+    lines=open(ff).readlines()
     if lines[0].strip().startswith('#!'):
-      print('   - fixing %s'%f)
-      lines[0]='#!'+sys.executable
-      open(f,'w').writelines(lines)
+      print('   - fixing %s'%ff)
+      lines[0]='#!'+sys.executable+'\n'
+      open(ff,'w').writelines(lines)
 
   # copy many folders:
   dirs='data','datasets','misc','nc','roms','util','ext'
@@ -51,16 +53,20 @@ def go():
       for d in dirs:
          if d!=d.lower() or d=='__pycache__':# or d=='ext':
            D=os.path.join(root,d)
-           print('   - remving %s'%D)
+           print('   - removing %s'%D)
            shutil.rmtree(D)
 
   # extensions:
   # why use subprocess instead of f2py.compile? See:
   # https://numpy.org/doc/2.1/f2py/usage.html
 
+  extDir=[] # folders to remove after compilation
+
   pwd=os.getcwd()
-  extdir=os.path.join(dest)
-  os.chdir(extdir)
+  soDir=dest
+  extDir+=[os.path.join(soDir,'ext')]
+  os.chdir(soDir)
+
 
   s=sys.executable,'-m','numpy.f2py','-c','ext/alg.f','-m','alg'
   subprocess.run(s)
@@ -69,11 +75,17 @@ def go():
   s=sys.executable,'-m','numpy.f2py','-c','ext/lu.f90','-m','lusolver'
   subprocess.run(s)
 
-  extdir=os.path.join(dest,'roms')
-  os.chdir(extdir)
+  soDir=os.path.join(dest,'roms')
+  extDir+=[os.path.join(soDir,'ext')]
+  os.chdir(soDir)
 
   s=sys.executable,'-m','numpy.f2py','-c','ext/rtools.f90','../ext/pppack.f90','-m','rtools'
   subprocess.run(s)
+
+  # remove ext folders no longer needed:
+  for e in extDir:
+    print('## removing %s'%e)
+    shutil.rmtree(e)
 
   os.chdir(pwd)
 
@@ -89,7 +101,7 @@ def go():
       print('#ERROR: %s*.so not created'%s)
       status=1
 
-  if not status: print('   enjoy okean')
+  if not status: print('\n   enjoy okean')
 
 
 
